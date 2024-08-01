ca79fcba06a7429139dcd2062dba9852a8b768c6
DL若影 <1843370860@qq.com>
2024-08-01 11:39:35 +0800

    wq

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,8 @@
 
 <script setup>
 //欢迎页
-console.log("%c欢迎！", "color: purple; font-weight: 1000; font-size: 24px;");
+// console.log("%c欢迎！", "color: purple; font-weight: 1000; font-size: 24px;");
 // console.log('https://github.com/syhy0612/lrsNotes');
 </script>
 <style lang="scss" scoped>
-
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,13 +1,36 @@
 <template>
-  <div>
-    这是主页呢
+  <div class="container">
+    <div class="bg">
+      <div class="title">这是标题</div>
+      <router-link to="/board">
+        <el-button type="primary">开始使用</el-button>
+      </router-link>
+
+    </div>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="scss" scoped>
+.bg {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  background-color: #20240427;
+  text-align: center;
+}
 
+.content {
+  text-align: center;
+}
+
+.title {
+  color: #000000;
+  font-size: 32px;
+  margin-top: 30vh;
+  margin-bottom: 20px;
+}
 </style>
