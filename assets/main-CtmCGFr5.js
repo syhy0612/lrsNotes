5a5e7138d6d98e0cfbaa85402f9a2be823385c5c
syhy0612 <syhy0612@users.noreply.github.com>
2024-08-18 14:53:37 +0000

    deploy: a39aae3f39a64f6b83919ba5d5f6965edd44e22f

--- assets/main-CtmCGFr5.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./board-CBHvtoEj.js";import{_ as a,o as s,c as t,b as o,e as c}from"./index-B7S4PJvM.js";/* empty css                 *//* empty css                  */const r={class:"bgColor"},_={class:""},n={__name:"main",setup(i){return(d,m)=>(s(),t("div",r,[o("div",_,[o("div",null,[c(e)])])]))}},B=a(n,[["__scopeId","data-v-a5b14095"]]);export{B as default};
