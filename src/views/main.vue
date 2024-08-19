329d279a818388b0c1fffcebf0af7361f08d35a2
DL若影 <1843370860@qq.com>
2024-08-19 22:09:17 +0800

    pc端  新增  左右居中对齐

--- src/views/main.vue
+++ src/views/main.vue
@@ -2,28 +2,61 @@
 
   <!--背景颜色-->
   <div class="bgColor">
-    <!--背景图片-->
-    <div class="">
-      <!--面板-->
-      <div>
+    <div class="outside">
+      <div class="board" :class="position">
+        <!--面板-->
         <Board/>
       </div>
+      <!--按钮组-->
+      <el-radio-group v-model="position" size="large" class="layout-controls">
+        <el-radio-button label="left">左对齐</el-radio-button>
+        <el-radio-button label="center">居中</el-radio-button>
+        <el-radio-button label="right">右对齐</el-radio-button>
+      </el-radio-group>
     </div>
   </div>
-
 </template>
 
 <script setup>
-import Board from '../components/board.vue'
+import {ref} from 'vue';
+import Board from '../components/board.vue';
+import {ElRadioGroup, ElRadioButton} from 'element-plus';
 
+const position = ref('center');
 </script>
 
 <style lang="scss" scoped>
-.bgImg {
-  //background-image: url("../assets/bg.jpg");
-  height: 100%;
-  background-size: cover;
-  background-position: center;
-  //opacity: 0.15;
+.outside {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  height: 100vh; //使用视口高度
+  overflow: hidden; //防止出现滚动条
+}
+
+.board {
+  //border: 1px red solid; //todo
+  display: flex;
+  width: 100%;
+  margin-bottom: 20px;
+  flex-grow: 1; //让 Board 占据剩余空间
+}
+
+.board.left {
+  justify-content: flex-start;
+}
+
+.board.center {
+  justify-content: center;
+}
+
+.board.right {
+  justify-content: flex-end;
+}
+
+.layout-controls {
+  margin-top: auto; //将按钮组推到底部
 }
 </style>
