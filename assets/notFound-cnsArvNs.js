3466d2d3f014c0cbe7d430788f8a82183566fb82
syhy0612 <syhy0612@users.noreply.github.com>
2024-09-02 03:52:45 +0000

    deploy: fd403cbb7c82a26c3c2c3a0096ec265103458b9b

---
+++ assets/notFound-cnsArvNs.js
@@ -0,0 +1,1 @@
+import{_ as s,o as e,c as t,U as a,V as _,b as o}from"./index-Bwl5y8a0.js";const d={},n=c=>(a("data-v-15c1f4c4"),c=c(),_(),c),r={class:"container"},i=n(()=>o("div",{class:"bgColor"},[o("div",{class:"title"},"你来到了知识的荒漠")],-1)),p=[i];function l(c,f){return e(),t("div",r,p)}const u=s(d,[["render",l],["__scopeId","data-v-15c1f4c4"]]);export{u as default};
